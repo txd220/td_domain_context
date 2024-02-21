f9602c1cc956008b8ee9e88a8bacfc32c1b5449f
Timothy A. DeWees <timothy.dewees@sherwin.com>
2024-02-21 08:15:34 -0500

    AVRO schema array broken.  Goign to ask for help

---
+++ validate_avro.py
@@ -0,0 +1,24 @@
+import click
+from avro_validator.schema import Schema
+
+
+
+def validate_schema(schema_file):
+    with open(schema_file) as fo:
+        scema_data = fo.read()
+        schema = Schema(scema_data)
+        parsed_schema = schema.parse()
+        return parsed_schema
+
+@click.command()
+@click.option('--type', default='schema', help='[schema|data]')
+@click.option('--schema-file', default='schema.avsc', help='File for the AVRO schema')
+@click.option('--data-file', default='data.avro', help='Avro file being validated')
+def run(type: str = "schema", schema_file: str = "", data_file: str = ""):
+    schema = validate_schema(schema_file)
+    if type == 'data':
+        schema.validate(data_file)
+    
+
+if __name__ == '__main__':
+    run()
